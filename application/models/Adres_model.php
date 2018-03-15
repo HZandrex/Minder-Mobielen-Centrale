2363804f57be2aef86161c881beff876c4fc7399
geffrew <geffreyw2@hotmail.com>
2018-03-15 19:09:26 +0100

    modelnamen aangepast

---
+++ application/models/Adres_model.php
@@ -0,0 +1,29 @@
+<?php
+
+class adres_model extends CI_Model {
+
+    // +----------------------------------------------------------
+    // | MMC - adres_model.php
+    // +----------------------------------------------------------
+    // | 2 ITF - 2017-2018
+    // +----------------------------------------------------------
+    // | Model voor adres gegevens.
+    // |
+    // +----------------------------------------------------------
+    // | Thomas More Kempen
+    // +----------------------------------------------------------
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+	//Functie moet nog aangepast worden. Zorgen dat men de gegevens van de ingelogde persoon toont.
+    function getById($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get('adres');
+        return $query->row();
+    }
+                        
+}
---
+++ application/models/FunctieGebruiker_model.php
@@ -0,0 +1,41 @@
+<?php
+
+class FunctieGebruiker_model extends CI_Model {
+
+    // +----------------------------------------------------------
+    // | Lekkerbier - Bestelling_model.php
+    // +----------------------------------------------------------
+    // | 2 ITF - 201x-201x
+    // +----------------------------------------------------------
+    // | Bestelling model
+    // |
+    // +----------------------------------------------------------
+    // | Thomas More Kempen
+    // +----------------------------------------------------------
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function get($gebruikersId)
+    {
+        $this->db->where('GebruikersId', $gebruikersId);
+        $query = $this->db->get('functiegebruiker');
+        return $query->result();
+    }
+    function getWithName($gebruikersId)
+    {
+        $this->db->where('GebruikersId', $gebruikersId);
+        $query = $this->db->get('functiegebruiker');
+        $functiesGebruiker = $query->result();
+        
+        $this->load->model('functie_model');
+        foreach ($functiesGebruiker as $functieGebruiker) {
+            $functieGebruiker->functieNaam = $this->functie_model->get($functieGebruiker->FunctiesId);
+        }
+        
+        return $functiesGebruiker;
+    }
+                        
+}
