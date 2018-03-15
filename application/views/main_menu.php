cb1a108ba161ea992ed6270e34db35d8214b2a22
geffrew <geffreyw2@hotmail.com>
2018-03-15 21:47:03 +0100

    dropdown gemaakt

--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -5,23 +5,31 @@ echo divAnchor('home', 'Home', 'class="nav-item nav-link"');
 if ($gebruiker == null) { // niet aangemeld
     echo '</div>';
     echo '<div class="navbar-nav">';
-        echo divAnchor('gebruiker/inloggen', 'Inloggen', 'class="nav-item nav-link"');
+    echo divAnchor('gebruiker/inloggen', 'Inloggen', 'class="nav-item nav-link"');
     echo '</div>';
 } else { // wel aangemeld
     foreach ($gebruiker->functies as $functie) {
         switch ($functie->functieNaam->naam) {
-        case 'Coach':
-            echo divAnchor('product/bestel', 'Producten bestellen', 'class="nav-item nav-link"');
-            break;
-        
-        case 'Admin':
-            echo divAnchor('admin/webinfo', 'Webinfo wijzigen', 'class="nav-item nav-link"');
-            break;
+            case 'Coach':
+                echo divAnchor('product/bestel', 'Producten bestellen', 'class="nav-item nav-link"');
+                break;
+
+            case 'Admin':
+                echo divAnchor('admin/webinfo', 'Webinfo wijzigen', 'class="nav-item nav-link"');
+                break;
         }
     }
     echo '</div>';
-    
-    echo '<div class="navbar-nav">';
-        echo divAnchor('gebruiker/inloggen/loguit', $gebruiker->voornaam . ' ' . $gebruiker->naam, 'class="nav-item nav-link"');
-    echo '</div>';
-}
+    ?>
+    <div class="navbar-nav">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $gebruiker->voornaam . ' ' . $gebruiker->naam; ?>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <?php echo divAnchor('gebruiker/persoonlijkegegevens', 'Mijn gegevens', 'class="nav-link"'); ?>
+                <?php echo divAnchor('gebruiker/inloggen/loguit', 'Uitloggen', 'class="nav-link"'); ?>
+            </div>
+        </li>
+    </div>
+<?php } ?>
