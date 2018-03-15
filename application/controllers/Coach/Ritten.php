a20fbe239780f8b6f0840f228882a723e627b752
Lorenz Cleymans <lorenzcleymans@hotmail.com>
2018-03-15 16:33:48 +0100

    Lorenz heeft aanpassingen geedaan aan ritten.php voor coach en ook een view aangemaakt voor de coach

---
+++ application/controllers/Coach/Ritten.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ritten extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data['titel'] = 'Ritten';
+        $data['author'] = 'Lorenz .C';
+        
+        $data['gebruiker'] = $this->authex->getGebruikerInfo();
+ 
+        $this->load->model('ritten_model');
+        $data['ritten'] = $this->ritten_model->getById(114);
+
+        $partials = array('menu' => 'main_menu','inhoud' => 'Coach/ritten');
+        $this->template->load('main_master', $partials, $data);
+    }
+
+
+	
+}
+
---
+++ application/views/Coach/ritten.php
@@ -0,0 +1,55 @@
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Datum</th>
+      <th scope="col">Vertrek</th>
+        <th scope="col">Voor</th>
+      <th scope="col">Start Adres</th>
+	  <th scope="col">Eind Adres</th>
+	  <th scope="col">Terugrit</th>
+	  <th scope="col">Afstand prijs</th>
+	  <th scope="col">Extra kost</th>
+	  <th scope="col">Totaal kost</th>
+	  <th scope="col">Status</th>
+	  <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+	<?php
+		foreach($ritten as $rit){
+			if(!empty($rit)){
+	?>
+				<tr>
+					<td><?php print date("d.m.y", strtotime($rit->heenvertrek->tijd));?></td>
+					<td><?php print date("G:i", strtotime($rit->heenvertrek->tijd));?></td>
+                    <td><?php print $rit->mmId;?></td>
+					<td><?php print $rit->heenvertrek->adres->straat . " " . $rit->heenvertrek->adres->huisnummer;?></td>
+					<td><?php print $rit->heenaankomst->adres->straat . " " . $rit->heenaankomst->adres->huisnummer;?></td>
+					<td><?php if(!empty($rit->terugvertrek)){
+						print date("G:i", strtotime($rit->terugvertrek->tijd));
+					}else{
+						print "N/A";
+					} ?></td>
+					<td><?php print $rit->prijs;?>€</td>
+					<td><?php 
+						if(!empty($rit->extraKost)){
+							print $rit->extraKost . "€";
+						}?>
+					</td>
+					<td><?php print (intval($rit->prijs) + intval($rit->extraKost));?>€</td>
+					<td><?php print $rit->status->status->naam ?></td>
+					<td><?php print anchor('url', '<i class="fa fa-eye fa-2x"></i>'); ?></td>
+				</tr>
+	<?php
+			}
+		}
+	
+	?>
+  </tbody>
+</table>
+
+<?php
+
+
+
+?>
