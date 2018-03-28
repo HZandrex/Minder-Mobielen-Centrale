5078d6f5067a57dd6c09e504efae18fdc3a64458
geffrew <geffreyw2@hotmail.com>
2018-03-28 21:17:28 +0200

    GebruikersBeheren = gestart

---
+++ application/views/Medewerker/gebruikersBeherenOverzicht.php
@@ -0,0 +1,1 @@
+<h1 class="text-danger">To be continued!!!</h1>
--- application/views/main_menu.php
+++ application/views/main_menu.php
@@ -20,15 +20,17 @@ if ($gebruiker == null) { // niet aangemeld
     foreach ($gebruiker->functies as $functie) {
         switch ($functie->naam) {
             case 'Coach':
-                echo divAnchor('product/bestel', 'Producten bestellen', 'class="nav-item nav-link"');
+                echo divAnchor('Coach/Ritten','MM beheren','class="nav-item nav-link"');
+                break;
+
+            case 'Medewerker':
+                echo divAnchor('medewerker/gebruikersBeheren','Gebruikers beheren','class="nav-item nav-link"');
                 break;
 
             case 'Admin':
                 echo divAnchor('admin/webinfo', 'Webinfo wijzigen', 'class="nav-item nav-link"');
                 echo divAnchor('admin/instellingen', 'Instellingen', 'class="nav-item nav-link"');
                 break;
-            case 'Coach':
-                echo divAnchor('Coach/Ritten','MM beheren','class="nav-item nav-link"');
         }
     }
     echo '</div>';
